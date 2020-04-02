5eb2f81d3d4d85441459f7f481c2c153c0a1b4a0
Scott Morrison <scottmorr75@gmail.com>
2020-04-02 00:06:04 -0600

    adding CSS

--- script.js
+++ script.js
@@ -3,26 +3,28 @@
 $(document).ready(function () {
 
 
+    var currentDay = moment().format('LL');
+    $("#currentDay").append(currentDay);
+ 
+
+  // $(currentCity).text("Current City");   Doesn't work
 
 
+  $(".card-text").text("Next Day");
+    // var APIKey = "4c977f45a07a9d54331ea1e40d5a5185";
+    // var queryURL
+
+    // //https://openweathermap.org/appid
 
 
-
-
-    var APIKey = "4c977f45a07a9d54331ea1e40d5a5185";
-    var queryURL
-
-    //https://openweathermap.org/appid
-
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-        // $("#query-input").text(JSON.stringify(response));
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    //   }).then(function(response) {
+    //     // $("#query-input").text(JSON.stringify(response));
 
       
-      });
+    //   });
 
 
 
